d390c21afe2838d73d937ad21496d6f8163e2b77
josephthejoe <joseph.patenaude1@gmail.com>
2024-07-24 03:11:20 +0000

    added isprime web function

--- cmd/littletools/main.go
+++ cmd/littletools/main.go
@@ -4,6 +4,8 @@ import (
     "fmt"
     "html/template"
     "net/http"
+    "strings"
+    "strconv"
 //    "log"
 
     "github.com/gorilla/mux"
@@ -14,6 +16,19 @@ import (
 //    Name string
 //}
 /// Database initialization function
+
+func isIntPrime(num int) bool {
+    if num == 0 || num == 1 {
+        return false
+    }
+
+    for i:=2; i<num; i++ {
+        if num % i == 0 {
+             return false
+            }
+    }
+    return true
+}
 
 func main() {
     // Create a new router
@@ -36,9 +51,51 @@ func main() {
         }
     }
 
+    // Define a handler function for the home page
+    isPrimeHandler := func(w http.ResponseWriter, r *http.Request) {
+        p := ""
+        // Check if the request is a POST request
+        if r.Method == http.MethodPost {
+            // Parse the form data
+            err := r.ParseForm()
+            if err != nil {
+                http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+                return
+            }
+            num := strings.TrimSpace(r.Form.Get("check"))
+            intNum, err := strconv.Atoi(num)
+            if err != nil {
+                http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+                return
+            }
+            check := isIntPrime(intNum)
+            if check == false {
+                p = "the numer is NOT prime"
+            } else {
+                p = "the number is prime"
+            }
+        }
+
+//        if r.Method == http.MethodGet {
+//            p = ""
+//        }
+        // Parse the HTML template
+        tmpl, err := template.ParseFiles("./web/templates/isprime.html")
+        if err != nil {
+            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+            return
+        }
+
+        // Execute the template
+        err = tmpl.Execute(w, p)
+        if err != nil {
+            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+            return
+        }
+    }
     // Register the handlers for the home, greet, and login pages
     router.HandleFunc("/", homeHandler).Methods(http.MethodGet)
-    //router.HandleFunc("/updatehost", updateHostHandler).Methods(http.MethodGet, http.MethodPost)
+    router.HandleFunc("/isprime", isPrimeHandler).Methods(http.MethodGet, http.MethodPost)
 
     // Serve static files from the "static" directory
     //router.PathPrefix("./web/static/").Handler(http.StripPrefix("./web/static/", http.FileServer(http.Dir("static"))))
